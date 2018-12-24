4dfaa7f110d4182b21fbbd365a11eeb053a25cea
PYCheng <hsinshengsouth@gmail.com>
2018-12-24 15:04:47 +0800

    優惠卷Model 新增service和DAO

--- src/com/coupon/model/CouponDAO.java
+++ src/com/coupon/model/CouponDAO.java
@@ -1,21 +1,30 @@
 package com.coupon.model;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import tool.BLOB;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
-public class CouponJDBCDAO implements CouponDAO_interface{
 
-	private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
-	private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
-	private static final String USER = "CA105G4"; 
-	private static final String PASSWORD = "123456"; 
+public class CouponDAO implements CouponDAO_interface{
+
+private static DataSource ds =null;	
+	
+	static {
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/CA105G4DB");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} 
+	}
 	
     private static final String INSERT_SQL = "INSERT INTO Coupon(cpnID,cpnPic,discount,quantity,appquantity) VALUES ('C'||LPAD(to_char(cpn_seq.NEXTVAL), 4, '0'), ?, ?, ?, ?)";
     private static final String UPDATE_SQL = "UPDATE Coupon set cpnPic=? discount=? quantity=? appQuantity = ? where cpnID = ?";
@@ -23,13 +32,6 @@ public class CouponJDBCDAO implements CouponDAO_interface{
     private static final String GET_ONE_SQL = "SELECT * from coupon where cpnID = ?";
     
 	
-    static {
-    	try {
-			Class.forName(DRIVER);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}    	
-    }
 	
 	@Override
 	public void insert(CouponVO couponVO) {
@@ -37,7 +39,7 @@ public class CouponJDBCDAO implements CouponDAO_interface{
 		PreparedStatement pstmt = null;
 		
 		try {
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			con=ds.getConnection();
 			pstmt = con.prepareStatement(INSERT_SQL);
 			
 			pstmt.setBytes(1, couponVO.getCpnPic());
@@ -74,7 +76,7 @@ public class CouponJDBCDAO implements CouponDAO_interface{
 		PreparedStatement pstmt = null;
 		
 		try {
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			con=ds.getConnection();
 			pstmt = con.prepareStatement(UPDATE_SQL);
 			
 			pstmt.setBytes(1, couponVO.getCpnPic());
@@ -115,7 +117,7 @@ public class CouponJDBCDAO implements CouponDAO_interface{
 		ResultSet rs = null;
 		
 		try {
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			con=ds.getConnection();
 			pstmt = con.prepareStatement(GET_ONE_SQL);
 			
 			pstmt.setString(1, cpnID);
@@ -171,7 +173,7 @@ public class CouponJDBCDAO implements CouponDAO_interface{
 		ResultSet rs = null;
 		
 		try {
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			con=ds.getConnection();
 			pstmt = con.prepareStatement(GET_ALL_SQL);
 			rs = pstmt.executeQuery();
 			
@@ -218,51 +220,4 @@ public class CouponJDBCDAO implements CouponDAO_interface{
 		return list;
 	}
 
-	public static void main(String[] args) {
-		CouponJDBCDAO dao = new CouponJDBCDAO();
-		
-		// 新增
-		CouponVO couponVO1 = new CouponVO();
-		couponVO1.setCpnPic(new BLOB().writeBlob("images/logo5.png"));
-		couponVO1.setDiscount(500);
-		couponVO1.setQuantity(20);
-		couponVO1.setAppQuantity(20);
-		
-		dao.insert(couponVO1);
-		System.out.println("新增成功");
-		System.out.println("------------------");
-		
-		// 修改
-		CouponVO couponVO2 = new CouponVO();
-		
-		couponVO2.setAppQuantity(10);
-		couponVO2.setCpnID("C0002");
-		dao.update(couponVO2);
-		
-		System.out.println("修改成功");
-		System.out.println("--------------------");		
-		
-		// 查詢單筆
-		CouponVO couponVO3 = dao.findByPK("C0003");
-		
-		System.out.println(couponVO3.getCpnID());
-		System.out.println(couponVO3.getDiscount());
-		System.out.println(couponVO3.getQuantity());
-		System.out.println(couponVO3.getAppQuantity());
-		
-		System.out.println("查詢單筆成功");
-		System.out.println("------------------");
-		
-		// 查詢全部
-		List<CouponVO> list = dao.getAll();
-		for(CouponVO aCouponVO : list) {
-			System.out.println(aCouponVO.getCpnID());
-			System.out.println(aCouponVO.getDiscount());
-			System.out.println(aCouponVO.getQuantity());
-			System.out.println(aCouponVO.getAppQuantity());
-			System.out.println();
-		}
-		System.out.println("查詢全部成功");
-		System.out.println("-------------------");
 	}
-}
