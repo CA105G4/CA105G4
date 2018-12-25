8203603f2f7cbf042dc670060fcb36f959071721
chad <chad9043@gmail.com>
2018-12-25 21:53:09 +0800

    [新增] MemberDAO 和 MemberService

---
+++ src/com/member/model/MemberService.java
@@ -0,0 +1,68 @@
+package com.member.model;
+
+import java.sql.Date;
+import java.util.List;
+
+
+
+public class MemberService {
+	private MemberDAO_interface dao;
+
+	public MemberService() {
+		dao = new MemberJDBCDAO();
+}
+	public MemberVO addMem(String memName,String memAcc,String memPsw,Date memBirth,String memEmail,String memTel,String memAddr,String memSex,String memSkill,byte[]memPic,String memIDcard) {
+
+		MemberVO memberVO = new MemberVO();
+		
+		
+		memberVO.setMemName(memName);
+		memberVO.setMemAcc(memAcc);
+		memberVO.setMemPsw(memPsw);
+		memberVO.setMemBirth(memBirth);
+		memberVO.setMemEmail(memEmail);
+		memberVO.setMemTel(memTel);
+		memberVO.setMemAddr(memAddr);
+		memberVO.setMemSex(memSex);
+		memberVO.setMemSkill(memSkill);
+		memberVO.setMemPic(memPic);
+		memberVO.setMemIDcard(memIDcard);
+		
+		dao.insert(memberVO);
+
+		return memberVO;
+	}
+
+	public MemberVO updateMem(String memID,String memName,String memAcc,String memPsw,Date memBirth,String memEmail,String memTel,String memAddr,String memSex,String memSkill,Integer memState,byte[]memPic,String memIDcard) {
+
+		MemberVO memberVO = new MemberVO();
+		
+		memberVO.setMemID(memID);
+		memberVO.setMemName(memName);
+		memberVO.setMemAcc(memAcc);
+		memberVO.setMemPsw(memPsw);
+		memberVO.setMemBirth(memBirth);
+		memberVO.setMemEmail(memEmail);
+		memberVO.setMemTel(memTel);
+		memberVO.setMemAddr(memAddr);
+		memberVO.setMemSex(memSex);
+		memberVO.setMemSkill(memSkill);
+		memberVO.setMemState(memState);
+		
+		memberVO.setMemIDcard(memIDcard);
+		
+		dao.update(memberVO);
+
+		return memberVO;
+	}
+
+	
+
+	public MemberVO getOneMem(String memID) {
+		return dao.findByPK(memID);
+	}
+
+	public List<MemberVO> getAll() {
+		return dao.getAll();
+	}
+}
